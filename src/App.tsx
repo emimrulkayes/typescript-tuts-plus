c137ec24a487f826dd4c6fad17f6ff02a3a09b94
emimrulkayes <emimrulkayes@gmail.com>
2023-03-01 05:24:18 +0600

    Added Docs Folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      
+        <h2>Welcome to Typescript Series...</h2>
       </header>
     </div>
   );
